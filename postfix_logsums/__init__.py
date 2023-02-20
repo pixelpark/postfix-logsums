916febc80ab8aa616026c8c9393268295bc5d2ab
Frank Brehm <frank@brehm-online.com>
2023-02-20 09:49:01 +0100

    Adding postfix_logsums/__init__.py

---
+++ postfix_logsums/__init__.py
@@ -0,0 +1,13 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@summary: A log analyzer/summarizer for the Postfix MTA.
+
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2023 by Frank Brehm, Berlin
+"""
+
+__version__ = '0.1.0'
+
+# vim: ts=4 et list
