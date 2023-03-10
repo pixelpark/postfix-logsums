368275ab8d9441cd4b6dccbef238539bba99ef6c
Frank Brehm <frank@brehm-online.com>
2023-03-10 14:12:45 +0100

    Completing test/test_errors.py

--- test/test_errors.py
+++ test/test_errors.py
@@ -73,6 +73,98 @@ class TestErrors(PostfixLogsumsTestcase):
         LOG.debug("%s raised: %s", e.__class__.__name__, e)
         self.assertEqual(test_txt, str(e))
 
+    # -------------------------------------------------------------------------
+    def test_attribute_errors(self):
+
+        wrong_attr = 'uhu'
+
+        LOG.info("Test raising a WrongMsgStatsAttributeError ...")
+
+        from postfix_logsums.errors import PostfixLogsumsError
+        from postfix_logsums.errors import WrongMsgStatsAttributeError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise WrongMsgStatsAttributeError(wrong_attr, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'WrongMsgStatsAttributeError')
+
+        LOG.info("Test raising a WrongMsgStatsValueError ...")
+
+        from postfix_logsums.errors import WrongMsgStatsValueError
+
+        msg = "Wrong value {v!r} in test_attribute_errors() of {c}.".format(
+            v=wrong_attr, c=self.__class__.__name__)
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise WrongMsgStatsValueError(msg)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'WrongMsgStatsValueError')
+
+    # -------------------------------------------------------------------------
+    def test_key_errors(self):
+
+        wrong_key = 'uhu'
+
+        LOG.info("Test raising a WrongMsgStatsKeyError ...")
+
+        from postfix_logsums.errors import PostfixLogsumsError
+        from postfix_logsums.errors import WrongMsgStatsKeyError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise WrongMsgStatsKeyError(wrong_key, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'WrongMsgStatsKeyError')
+
+        LOG.info("Test raising a WrongMsgPerDayKeyError ...")
+
+        from postfix_logsums.errors import WrongMsgPerDayKeyError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise WrongMsgPerDayKeyError(wrong_key, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'WrongMsgPerDayKeyError')
+
+    # -------------------------------------------------------------------------
+    def test_hour_errors(self):
+
+        wrong_hour = 'uhu'
+
+        LOG.info("Test raising a WrongMsgStatsHourError ...")
+
+        from postfix_logsums.errors import PostfixLogsumsError
+        from postfix_logsums.errors import WrongMsgStatsHourError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise WrongMsgStatsHourError(wrong_hour, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'WrongMsgStatsHourError')
+
+        LOG.info("Test raising a MsgStatsHourValNotfoundError ...")
+
+        from postfix_logsums.errors import MsgStatsHourValNotfoundError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise MsgStatsHourValNotfoundError(wrong_hour, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'MsgStatsHourValNotfoundError')
+
+        LOG.info("Test raising a MsgStatsHourInvalidMethodError ...")
+
+        from postfix_logsums.errors import MsgStatsHourInvalidMethodError
+
+        with self.assertRaises(PostfixLogsumsError) as cm:
+            raise MsgStatsHourInvalidMethodError(wrong_hour, self.__class__.__name__)
+        e = cm.exception
+        LOG.debug("%s raised: %s", e.__class__.__name__, e)
+        self.assertEqual(e.__class__.__name__, 'MsgStatsHourInvalidMethodError')
+
+
 # =============================================================================
 if __name__ == '__main__':
 
@@ -87,6 +179,9 @@ if __name__ == '__main__':
 
     suite.addTest(TestErrors('test_import', verbose))
     suite.addTest(TestErrors('test_general_errors', verbose))
+    suite.addTest(TestErrors('test_attribute_errors', verbose))
+    suite.addTest(TestErrors('test_key_errors', verbose))
+    suite.addTest(TestErrors('test_hour_errors', verbose))
 
     runner = unittest.TextTestRunner(verbosity=verbose)
 
