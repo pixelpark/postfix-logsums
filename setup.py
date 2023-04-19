63df6b011faaadae756ae96680fe0d3a0fd06c2e
Frank Brehm <frank@brehm-online.com>
2023-04-19 18:54:06 +0200

    Generating .mo-files on building Python package

--- setup.py
+++ setup.py
@@ -15,11 +15,16 @@ import os
 import sys
 import re
 import pprint
+import glob
+import subprocess
 
 from pathlib import Path
 
 # Third party modules
 from setuptools import setup
+from setuptools.command.sdist import sdist
+
+from babel.messages import frontend as babel
 
 # own modules:
 __module_name__ = 'postfix_logsums'
@@ -27,12 +32,16 @@ __setup_script__ = Path(__file__).resolve()
 __base_dir__ = __setup_script__.parent
 __module_dir__ = __base_dir__ / __module_name__
 __init_py__ = __module_dir__ / '__init__.py'
+__share_dir__ = Path(sys.base_prefix) / 'share'
+__locale_dir__ = __share_dir__ / 'locale'
 
 PATHS = {
     '__setup_script__': str(__setup_script__),
     '__base_dir__': str(__base_dir__),
     '__module_dir__': str(__module_dir__),
     '__init_py__': str(__init_py__),
+    '__share_dir__': __share_dir__,
+    '__locale_dir__': __locale_dir__,
 }
 
 def pp(obj):
@@ -160,11 +169,67 @@ __scripts__ = ['postfix-logsums']
 
 
 # -----------------------------------
+PO_FILES = 'locale/*/LC_MESSAGES/*.po'
+__package_data__ = {}
+__data_files__ = []
+
+def create_mo_files():
+    """Compile the translation files."""
+    mo_files = []
+    for po_path in glob.glob(PO_FILES):
+        mo = Path(po_path.replace('.po', '.mo'))
+        if not mo.exists():
+            subprocess.call(['msgfmt', '-o', str(mo), po_path])
+        mo_files.append(mo)
+
+    print("Found mo files: {}\n".format(pp(mo_files)))
+    return mo_files
+
+
+__pkg_mo_paths__ = create_mo_files()
+__pkg_mo_files__ = []
+for mo_file in __pkg_mo_paths__:
+    __pkg_mo_files__.append(str(mo_file))
+
+__package_data__[''] = __pkg_mo_files__
+print("Package_data:\n" + pp(__package_data__) + "\n")
+
+
+for mo_file in __pkg_mo_paths__:
+    ltype = mo_file.parent.name
+    lname = mo_file.parent.parent.name
+    ldir = __locale_dir__ / lname / ltype
+    mo_file_rel = str(mo_file).lstrip('/')
+    __data_files__.append((str(ldir), [mo_file_rel]))
+
+print("Found data files:\n" + pp(__data_files__) + "\n")
+
+
+# -----------------------------------
+class Sdist(sdist):
+    """Custom ``sdist`` command to ensure that mo files are always created."""
+
+    def run(self):
+        self.run_command('compile_catalog')
+        # sdist is an old style class so super cannot be used.
+        sdist.run(self)
+
+
+# -----------------------------------
 setup(
     version=__packet_version__,
     long_description=read(__readme_file__),
     scripts=__scripts__,
     requires=__requirements__,
+    package_data=__package_data__,
+    data_files=__data_files__,
+    cmdclass={
+        'compile_catalog': babel.compile_catalog,
+        'extract_messages': babel.extract_messages,
+        'init_catalog': babel.init_catalog,
+        'update_catalog': babel.update_catalog,
+        'sdist': Sdist,
+    },
 )
 
 
