7a5c4f364ef7f2451074fcf1bd3e0867955edda0
Frank Brehm <frank@brehm-online.com>
2026-03-17 09:36:24 +0100

    Updating update-env.sh for refactored virtual env.

--- update-env.sh
+++ update-env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2317  # Don't warn about unreachable commands in this file
 
 set -e
 set -u
@@ -7,25 +8,34 @@ VERBOSE="n"
 DEBUG="n"
 QUIET='n'
 
-VERSION="2.4"
+VERSION="3.0"
 
 # console colors:
 RED=""
 YELLOW=""
 GREEN=""
-# BLUE=""
+BLUE=""
+# shellcheck disable=SC2034
+MAGENTA=""
 CYAN=""
 NORMAL=""
 
-BASENAME=$( basename "${0}" )
+BASENAME=$(basename "${0}" )
 BASE_DIR=$( dirname "$0" )
 cd "${BASE_DIR}"
 BASE_DIR=$( readlink -f . )
+MAN_PARENT_DIR="data/share/man"
 
-declare -a VALID_PY_VERSIONS=("3.11" "3.10" "3.9" "3.8" "3.7" "3.6")
+declare -a VALID_PY_VERSIONS=("3.14" "3.13" "3.12" "3.11" "3.10" "3.9" "3.8")
+VENV='.venv'
 
 PIP_OPTIONS=
 export VIRTUAL_ENV_DISABLE_PROMPT=y
+
+TMPFILE_GETVALUE=
+TMPFILE_GETKEYS=
+ENTRYPOINTS=''
+DATA_DIR=
 
 #-------------------------------------------------------------------
 detect_color() {
@@ -55,17 +65,21 @@ detect_color() {
 
     # console colors:
     if [ "${use_color}" = "true" ] ; then
-        RED="\033[38;5;196m"
-        YELLOW="\033[38;5;226m"
-        GREEN="\033[38;5;46m"
-        # BLUE="\033[38;5;27m"
-        CYAN="\033[38;5;36m"
-        NORMAL="\033[39m"
+        RED=$( tput setaf 9 )
+        GREEN=$( tput setaf 10 )
+        YELLOW=$( tput setaf 11 )
+        BLUE=$( tput setaf 12 )
+        # shellcheck disable=SC2034
+        MAGENTA=$( tput setaf 13 )
+        CYAN=$( tput setaf 14 )
+        NORMAL=$( tput op )
     else
         RED=""
         YELLOW=""
         GREEN=""
-        # BLUE=""
+        BLUE=""
+        # shellcheck disable=SC2034
+        MAGENTA=""
         CYAN=""
         NORMAL=""
     fi
@@ -89,7 +103,7 @@ debug() {
     if [[ "${VERBOSE}" != "y" ]] ; then
         return 0
     fi
-    echo -e " * [$(my_date)] [${BASENAME}:${CYAN}DEBUG${NORMAL}]: $*" >&2
+    echo -e " * [$(my_date)] [${BASENAME}:${BLUE}DEBUG${NORMAL}]: $*" >&2
 }
 
 #------------------------------------------------------------------------------
@@ -202,7 +216,7 @@ get_options() {
                 RED=""
                 YELLOW=""
                 GREEN=""
-                # BLUE=""
+                BLUE=""
                 CYAN=""
                 NORMAL=""
                 shift
@@ -211,7 +225,7 @@ get_options() {
                 RED=""
                 YELLOW=""
                 GREEN=""
-                # BLUE=""
+                BLUE=""
                 CYAN=""
                 NORMAL=""
                 shift
@@ -249,15 +263,30 @@ get_options() {
     if type -t msgfmt >/dev/null ; then
         :
     else
-        echo "Command '${RED}msgfmt${NORMAL}' not found, please install package '${YELLOW}gettext${NORMAL}' or appropriate." >&2
+        echo -e "Command '${RED}msgfmt${NORMAL}' not found, please install package '${YELLOW}gettext${NORMAL}' or appropriate." >&2
         exit 6
     fi
 
     if [[ "${VERBOSE}" == "y" ]] ; then
         PIP_OPTIONS="--verbose"
     elif [[ "${QUIET}" == "y" ]] ; then
+        PIP_OPTIONS="--quiet --quiet"
+    else
         PIP_OPTIONS="--quiet"
     fi
+
+}
+
+#------------------------------------------------------------------------------
+RM() {
+
+    local cmd="rm"
+    if [[ "${VERBOSE}" == "y" ]] ; then
+        cmd+=" --verbose"
+    fi
+    debug "Executing: ${cmd} $*"
+    # shellcheck disable=SC2086,SC2294
+    eval ${cmd} "$@"
 
 }
 
@@ -270,11 +299,15 @@ init_venv() {
 
     empty_line
     line
-    info "Preparing virtual environment …"
+    info "Preparing and using virtual environment '${CYAN}${VENV}${NORMAL}' …"
     empty_line
 
 
-    if [[ ! -f venv/bin/activate ]] ; then
+    if [[ ! -f "${VENV}/bin/activate" ]] ; then
+        if [[ -d "${VENV}" ]] ; then
+            info "Removing '${CYAN}${VENV}${NORMAL}' …"
+            RM --recursive "${VENV}"
+        fi
         found="n"
         for py_version in "${VALID_PY_VERSIONS[@]}" ; do
             python="python${py_version}"
@@ -284,7 +317,7 @@ init_venv() {
                 empty_line
                 info "Found '${GREEN}${python}${NORMAL}'."
                 empty_line
-                "${python}" -m venv venv
+                "${python}" -m venv "${VENV}"
                 break
             fi
         done
@@ -298,35 +331,216 @@ init_venv() {
     fi
 
     # shellcheck disable=SC1091
-    . venv/bin/activate || exit 5
+    . "${VENV}/bin/activate" || exit 5
+
+}
+
+#------------------------------------------------------------------------------
+cleanup_awk_scripts() {
+
+    debug "Cleaning up AWK scripts ..."
+
+    if [[ -n "${TMPFILE_GETVALUE}" && -f "${TMPFILE_GETVALUE}" ]] ; then
+        debug "Removing '${CYAN}${TMPFILE_GETVALUE}${NORMAL}' ..."
+        RM "${TMPFILE_GETVALUE}"
+    fi
+
+    if [[ -n "${TMPFILE_GETKEYS}" && -f "${TMPFILE_GETKEYS}" ]] ; then
+        debug "Removing '${CYAN}${TMPFILE_GETKEYS}${NORMAL}' ..."
+        RM "${TMPFILE_GETKEYS}"
+    fi
+
+}
+
+#------------------------------------------------------------------------------
+create_awk_scripts() {
+
+    line
+    info "Creating AWK scripts for evaluating pyproject.toml."
+
+    TMPFILE_GETVALUE=$( mktemp 'get-inifile-value-XXXXXXXX.awk' )
+    TMPFILE_GETKEYS=$( mktemp 'get-inifile-keys-XXXXXXXX.awk' )
+
+    trap cleanup_awk_scripts INT TERM EXIT ABRT
+
+    lines=$( cat <<-EOF
+		# Script for extracting a value from an INI-file
+		#
+		# Example for calling:
+		# awk -f get_ini_value.awk -v target_section="[build-system]" -v key="build-backend" pyproject.toml
+
+		BEGIN {
+		  # Defaults
+		  found_section = 0
+		}
+
+		{
+		  # Strip leading and trailing white spaces
+		  line = \$0
+		  gsub(/^[ \t]+|[ \t]+\$/, "", line)
+
+		  # Step comments and empty rows
+		  if (line ~ /^;/ || line ~ /^#/) {
+		    next
+		  }
+		  if (length(line) == 0) {
+		    next
+		  }
+
+		  # Check for the correct section
+		  if (line ~ /^\[.*\]\$/) {
+		    section = substr(line, 2, length(line) - 2)
+		    if (section == target_section) {
+		      found_section = 1
+		    } else {
+		      found_section = 0
+		    }
+		    next
+		  }
+
+		  # If we are in the correct section
+		  if (found_section == 1) {
+		    if (line ~ "^" key "[ \t]*=.*") {
+		      split(line, arr, /[ \t]*=[ \t]*/)
+
+		      value = arr[2]
+
+		      gsub(/^"|"\$/, "", value)
+		      gsub(/^'|'\$/, "", value)
+
+		      print value
+
+		      exit
+		    }
+		  }
+		}
+		EOF
+    )
+
+    echo "${lines}" > "${TMPFILE_GETVALUE}"
+    debug "Created '${CYAN}${TMPFILE_GETVALUE}${NORMAL}'."
+
+    lines=$( cat <<-EOF
+		# Script for extracting all keys of a section of an INI-file
+		#
+		# Example for calling:
+		# awk -f get_ini_keys.awk -v target_section="project.scripts" pyproject.toml
+
+		BEGIN {
+		    in_section = 0
+		}
+
+		{
+		    # Cleaning row: removing leading and trailing whitespaces and cut before comment character
+		    line = \$0
+		    sub(/^[ \t]+/, "", line)
+		    sub(/[ \t]+\$/, "", line)
+		    sub(/[ \t]*[#;].*\$/, "", line)
+
+		    # Skip on empty row
+		    if (line == "") {
+		        next
+		    }
+
+		    # Check for section header
+		    if (line ~ /^\[.*\]\$/) {
+		        section = substr(line, 2, length(line) - 2)
+		        if (section == target_section) {
+		            in_section = 1
+		        } else {
+		            in_section = 0
+		        }
+		        next
+		    }
+
+		    # Perform key-value-pair in target section
+		    if (in_section == 1) {
+		        if (line ~ /^[a-zA-Z0-9_-]+[ \t]*=[ \t]*.*\$/) {
+		            # extract and print key
+		            split(line, a, /[ \t]*=[ \t]*/)
+		            print a[1]
+		        }
+		    }
+		}
+		EOF
+    )
+
+    echo "${lines}" > "${TMPFILE_GETKEYS}"
+    debug "Created '${CYAN}${TMPFILE_GETKEYS}${NORMAL}'."
 
 }
 
 #------------------------------------------------------------------------------
 upgrade_pip() {
+    empty_line
     line
     info "Upgrading PIP …"
     empty_line
+    # shellcheck disable=SC2086
     pip install ${PIP_OPTIONS} --upgrade --upgrade-strategy eager pip
     empty_line
 }
 
 #------------------------------------------------------------------------------
-upgrade_setuptools() {
-    line
-    info "Upgrading setuptools + wheel + six …"
+upgrade_flit() {
     empty_line
-    pip install ${PIP_OPTIONS} --upgrade --upgrade-strategy eager setuptools wheel six
+    line
+    info "Upgrading flit + wheel + six …"
+    empty_line
+    # shellcheck disable=SC2086
+    pip install ${PIP_OPTIONS} --upgrade --upgrade-strategy eager flit wheel six
     empty_line
 }
 
 #------------------------------------------------------------------------------
+install_modules_by_flit() {
+    empty_line
+    line
+    info "Installing all necessary packages with flit ..."
+    empty_line
+    flit install --only-deps --deps all --python "${VENV}/bin/python3"
+}
+
+#------------------------------------------------------------------------------
 upgrade_modules() {
+    local packages=''
+    local package=''
+
+    empty_line
     line
     info "Installing and/or upgrading necessary modules …"
     empty_line
-    pip install ${PIP_OPTIONS} --upgrade --upgrade-strategy eager --requirement requirements.txt
+    packages=$( pip3 list --outdated --exclude-editable --format json 2>/dev/null  | jq -r .[].name )
+
+    if [[ -n "${packages}" ]] ; then
+        msg="Packages to update:"
+        for package in ${packages} ; do
+            msg+="\n * ${CYAN}${package}${NORMAL}"
+        done
+        info "${msg}"
+        empty_line
+        # shellcheck disable=SC2086
+        pip install ${PIP_OPTIONS} --upgrade --upgrade-strategy eager ${packages}
+        empty_line
+    else
+        info "No packages to update found."
+    fi
+
+}
+
+#------------------------------------------------------------------------------
+install_local_package() {
+
     empty_line
+    line
+    info "Installing local package into '${CYAN}${VENV}${NORMAL}' …"
+    empty_line
+    info "Ensuring directory '${CYAN}${MAN_PARENT_DIR}${NORMAL}' …"
+    mkdir -pv "${MAN_PARENT_DIR}"
+
+    empty_line
+    flit install --symlink --deps production --python "${VENV}/bin/python3"
+
 }
 
 #------------------------------------------------------------------------------
@@ -334,6 +548,7 @@ list_modules() {
     if [[ "${QUIET}" == "y" ]] ; then
         return 0
     fi
+    empty_line
     line
     info "Installed modules:"
     empty_line
@@ -342,9 +557,88 @@ list_modules() {
 }
 
 #------------------------------------------------------------------------------
+eval_entrypoints() {
+
+    line
+    info "Get all entrypoints ..."
+    ENTRYPOINTS=''
+    local entrypoint
+    local cmd
+    local result
+
+    cmd="awk -f '${TMPFILE_GETKEYS}' -v target_section=project.scripts pyproject.toml"
+    debug "Calling ${cmd}"
+    # shellcheck disable=SC2086,SC2294
+    result=$( eval ${cmd} )
+    debug "Got raw entrypoints from AWK script: '${result}'"
+
+    if [[ -n "${result}" ]] ; then
+        debug "Evaluate entrypoints ..."
+        for entrypoint in ${result}; do
+            if [[ -n "${ENTRYPOINTS}" ]] ; then
+                ENTRYPOINTS+=" "
+            fi
+            ENTRYPOINTS+="${entrypoint}"
+        done
+    fi
+
+    if [[ -n "${ENTRYPOINTS}" ]] ; then
+        info "Found entrypoints: ${CYAN}${ENTRYPOINTS}${NORMAL}"
+    else
+        info "Did not found any entrypoints."
+    fi
+
+}
+
+#------------------------------------------------------------------------------
+eval_datadir() {
+
+    line
+    info "Get directory for external data."
+
+    DATA_DIR=''
+
+    cmd="awk -f '${TMPFILE_GETVALUE}' -v target_section=tool.flit.external-data -v key=directory pyproject.toml"
+    debug "Calling ${cmd}"
+    # shellcheck disable=SC2086,SC2294
+    DATA_DIR=$( eval ${cmd} )
+
+    if [[ -n "${DATA_DIR}" ]] ; then
+        info "Found data directory: '${CYAN}${DATA_DIR}${NORMAL}'"
+    else
+        info "Did not found data directory."
+    fi
+
+}
+
+#------------------------------------------------------------------------------
+ensure_datadir() {
+
+    if [[ -z "${DATA_DIR}" ]] ; then
+        return 0
+    fi
+
+    line
+    info "Ensuring directory for external data '${CYAN}${DATA_DIR}${NORMAL}' ..."
+
+    if [[ -e "${DATA_DIR}" ]] ; then
+        if [[ -d "${DATA_DIR}" ]] ; then
+            return 0
+        fi
+        error "Path exists, but is not a directory."
+        exit 7
+    fi
+
+    info "Creating directory '${CYAN}${DATA_DIR}${NORMAL}' ..."
+    mkdir --parent --verbose "${DATA_DIR}"
+
+}
+
+#------------------------------------------------------------------------------
 compile_i18n() {
 
     if [[ -x compile-xlate-msgs.sh ]]; then
+        empty_line
         line
         info "Updating i18n files in ${BASE_DIR} …"
         empty_line
@@ -363,10 +657,16 @@ compile_i18n() {
 main() {
 
     get_options "$@"
+    create_awk_scripts
+    eval_entrypoints
+    eval_datadir
+    ensure_datadir
     init_venv
     upgrade_pip
-    upgrade_setuptools
+    upgrade_flit
+    install_modules_by_flit
     upgrade_modules
+    install_local_package
     list_modules
     compile_i18n
 
