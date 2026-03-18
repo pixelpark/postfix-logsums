5be0fe4d97722b531ab87f206bc86d41b0d90cfa
Frank Brehm <frank@brehm-online.com>
2026-03-18 08:48:41 +0100

    Moving common used functions into new module src/postfix_logsums/functions.py

--- src/postfix_logsums/__init__.py
+++ src/postfix_logsums/__init__.py
@@ -18,13 +18,13 @@ import gzip
 import logging
 import lzma
 import os
-import pprint
 import re
-import shutil
 import sys
 
 # Own modules
 from .errors import PostfixLogsumsError
+from .functions import get_generic_appname
+from .functions import pp
 from .results import PostfixLogSums
 from .stats import MessageStats
 from .stats import MessageStatsPerDay
@@ -47,66 +47,6 @@ DEFAULT_MAX_TRIM_LENGTH = 66
 LOG = logging.getLogger(__name__)
 
 _ = XLATOR.gettext
-
-
-# =============================================================================
-def pp(value, indent=4, width=None, depth=None):
-    """
-    Return a pretty print string of the given value.
-
-    @return: pretty print string
-    @rtype: str
-    """
-    if not width:
-        term_size = shutil.get_terminal_size((DEFAULT_TERMINAL_WIDTH, DEFAULT_TERMINAL_HEIGHT))
-        width = term_size.columns
-
-    pretty_printer = pprint.PrettyPrinter(indent=indent, width=width, depth=depth)
-    return pretty_printer.pformat(value)
-
-
-# =============================================================================
-def encode_or_bust(obj, encoding="utf-8"):
-    """Convert given value to a byte string withe the given encoding."""
-    if isinstance(obj, str):
-        obj = obj.encode(encoding)
-
-    return obj
-
-
-# =============================================================================
-def to_bytes(obj, encoding="utf-8"):
-    """Do the same as encode_or_bust()."""
-    return encode_or_bust(obj, encoding)
-
-
-# =============================================================================
-def to_utf8(obj):
-    """Convert given value to a utf-8 encoded byte string."""
-    return encode_or_bust(obj, "utf-8")
-
-
-# =============================================================================
-def get_generic_appname(appname=None):
-    """Evaluate the current application name."""
-    if appname:
-        v = str(appname).strip()
-        if v:
-            return v
-    aname = sys.argv[0]
-    aname = re.sub(r"\.py$", "", aname, flags=re.IGNORECASE)
-    return os.path.basename(aname)
-
-
-# =============================================================================
-def get_smh(seconds):
-    """Get seconds, minutes and hours from seconds."""
-    hours = int(seconds / 3600)
-    seconds -= hours * 3600
-    minutes = int(seconds / 60)
-    seconds -= minutes * 60
-
-    return (seconds, minutes, hours)
 
 
 # =============================================================================
--- src/postfix_logsums/app.py
+++ src/postfix_logsums/app.py
@@ -46,17 +46,17 @@ from . import DEFAULT_TERMINAL_WIDTH
 from . import MAX_TERMINAL_WIDTH
 from . import PostfixLogParser
 from . import __version__ as GLOBAL_VERSION
-from . import get_generic_appname
-from . import get_smh
-from . import pp
-from . import to_bytes
+from .functions import get_generic_appname
+from .functions import get_smh
+from .functions import pp
+from .functions import to_bytes
 from .stats import HOURS_PER_DAY
 from .xlate import XLATOR
 from .xlate import format_list
 
 LOG = logging.getLogger(__name__)
 
-__version__ = "0.10.3"
+__version__ = "0.10.4"
 _ = XLATOR.gettext
 ngettext = XLATOR.ngettext
 
